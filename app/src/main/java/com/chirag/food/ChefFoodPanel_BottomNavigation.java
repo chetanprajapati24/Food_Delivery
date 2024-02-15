c865f6d5ca1a26bbb91f3d00db719171295c0e33
chetanprajapati24 <chiragprajapati24.cp@gmail.com>
2024-02-16 00:25:04 +0530

    commit

---
+++ app/src/main/java/com/chirag/food/ChefFoodPanel_BottomNavigation.java
@@ -0,0 +1,51 @@
+package com.chirag.food;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.chirag.food.chefFoodPanel.ChefHomeFragment;
+import com.chirag.food.chefFoodPanel.ChefOrderfragment;
+import com.chirag.food.chefFoodPanel.ChefPendingOrderFragment;
+import com.chirag.food.chefFoodPanel.ChefProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ChefFoodPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_food_panel_bottom_navigation);
+        BottomNavigationView navigationView =findViewById(R.id.chef_bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment =null;
+        if (item.getItemId() == R.id.chefHome) {
+            fragment = new ChefHomeFragment();
+        } else if (item.getItemId() == R.id.PendingOrders) {
+            fragment = new ChefPendingOrderFragment();
+        } else if (item.getItemId() == R.id.Orders) {
+            fragment = new ChefOrderfragment();
+        } else if (item.getItemId() == R.id.chefProfile) {
+            fragment = new ChefProfileFragment();
+        }
+        return loadcheffragment(fragment);
+    }
+
+    private boolean loadcheffragment(Fragment fragment) {
+        if(fragment!=null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_container ,fragment).commit();
+            return true;
+        }
+        return false;
+    }
+}

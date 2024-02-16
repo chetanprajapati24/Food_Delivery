e123dd14392ec3cdf2c9c73c3ad11097e3c37042
chetanprajapati24 <chiragprajapati24.cp@gmail.com>
2024-02-17 03:23:38 +0530

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,10 +43,15 @@ dependencies {
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.google.firebase:firebase-messaging:23.4.1")
     implementation("androidx.cardview:cardview:1.0.0")
-   // implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+   implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("com.hbb20:ccp:2.5.1")
+   //implementation ("com.theartofdev.edmodo:android-image-cropper:2.8.1")
+    implementation("com.vanniktech:android-image-cropper:4.5.0")
+    implementation ("com.squareup.picasso:picasso:2.5.2")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 buildscript {
+        repositories {
+            google()
+            mavenCentral()
+
+        }
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:4.4.1")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.

d2218d471448700b42ecac0c01146150d7dc1453
chetanprajapati24 <chiragprajapati24.cp@gmail.com>
2024-02-25 10:48:46 +0530

    commit

--- app/src/main/java/com/chirag/food/CustomerFoofPanel_BottomNavigation.java
+++ app/src/main/java/com/chirag/food/CustomerFoofPanel_BottomNavigation.java
@@ -1,14 +1,54 @@
 package com.chirag.food;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class CustomerFoofPanel_BottomNavigation extends AppCompatActivity {
+import com.chirag.food.customerFoodPanel.CustomerCartFragmnet;
+import com.chirag.food.customerFoodPanel.CustomerHomeFragment;
+import com.chirag.food.customerFoodPanel.CustomerOrderFragment;
+import com.chirag.food.customerFoodPanel.CustomerProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class CustomerFoofPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_foof_panel_bottom_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        if (item.getItemId() == R.id.cust_Home) {
+            fragment = new CustomerHomeFragment();
+        } else if (item.getItemId() == R.id.cart) {
+            fragment = new CustomerCartFragmnet();
+        } else if (item.getItemId() == R.id.cust_profile) {
+            fragment = new CustomerProfileFragment();
+        } else if (item.getItemId() == R.id.Cust_order) {
+            fragment = new CustomerCartFragmnet();
+        }else if (item.getItemId() == R.id.cart) {
+            fragment = new CustomerOrderFragment();
+        }
+
+        return loadcheffragment(fragment);
+    }
+
+    private boolean loadcheffragment(Fragment fragment) {
+
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/chirag/food/DeliveryFoodPanel_BottomNavigation.java
+++ app/src/main/java/com/chirag/food/DeliveryFoodPanel_BottomNavigation.java
@@ -1,14 +1,47 @@
 package com.chirag.food;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class DeliveryFoodPanel_BottomNavigation extends AppCompatActivity {
+
+import com.chirag.food.deliveryFoodPanel.DeliveryPendingOrdersFragment;
+import com.chirag.food.deliveryFoodPanel.DeliveryShippingOrdersFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class DeliveryFoodPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery_food_panel_bottom_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.delivery_bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        if (item.getItemId() == R.id.shiporders) {
+            fragment = new DeliveryShippingOrdersFragment();
+        } else if (item.getItemId() == R.id.pendingorders) {
+            fragment = new DeliveryPendingOrdersFragment();
+        }
+
+        return loaddeliveryfragment(fragment);
+    }
+
+    private boolean loaddeliveryfragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return true;
+        }
+        return false;
     }
 }

597c07a958566dfe92d8533b752ef86fdcdae800
chetanprajapati24 <chiragprajapati24.cp@gmail.com>
2024-02-25 10:50:54 +0530

    commit

---
+++ app/src/main/java/com/chirag/food/deliveryFoodPanel/DeliveryShippingOrdersFragment.java
@@ -0,0 +1,24 @@
+package com.chirag.food.deliveryFoodPanel;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.chirag.food.R;
+
+
+public class DeliveryShippingOrdersFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_delivery_shipping_order, null);
+        getActivity().setTitle("Ship Orders");
+        return v;
+    }
+}
